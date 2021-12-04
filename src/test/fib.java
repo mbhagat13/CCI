44bcfbe23f906dba0294e073e80c4a267188c23d
mbhagat13 <meherbhagat97@gmail.com>
2021-12-04 13:47:02 -0500

    adding fib

--- src/test/fib.java
+++ src/test/fib.java
@@ -1,4 +1,48 @@
 package test;
 
 public class fib {
+
+    public static int fib(int n){
+
+        if(n == 0){
+            return 0;
+        }else if (n == 1){
+            return 1;
+        }else{
+            return fib(n-2)+ fib(n-1);
+        }
+
+    }
+
+
+
+    public static int fib (int n, int[] memo){
+        if(n == 0){
+            return 0;
+        }else if (n == 1){
+            return 1;
+        }else if (memo[n] != 0){
+            return memo[n];
+        }else{
+            memo[n] =fib(n-2)+ fib(n-1);
+            return memo[n];
+        }
+    }
+
+
+
+
+    public static void main (String [] args){
+
+        int n = 10;
+        System.out.println(fib(n));
+
+        int [] m = new int [n + 1];
+
+        System.out.println(fib(n,m));
+
+
+
+    }
+
 }
